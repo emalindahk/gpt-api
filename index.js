1d2bd743d5d3cceeca03ab20ec655e10b8b97d14
emalindahk <emalindahkimari@gmail.com>
2025-11-25 19:52:18 +0300

    Fix vercel deploy

--- index.js
+++ index.js
@@ -43,8 +43,11 @@ app.post("/questions", async (req, res) => {
 });
 
 const PORT = process.env.PORT || 8000;
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
-});
+
+if (require.main === module) {
+  app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+  });
+}
 
 module.exports = app;
